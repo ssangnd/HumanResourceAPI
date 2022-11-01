7798e5c1892e1887f95bd5e5151882662f125726
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-11-01 22:12:25 +0700

    init and config structure project

---
+++ HumanResource.Infrastructure/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq.Expressions;
+
+
+namespace HumanResource.Infrastructure
+{
+    public interface IRepositoryBase<T,K> where T :class
+    {
+        T FindById(K id, params Expression<Func<T, object>>[] includeProperties);
+        Task<T> FindByIdAsync(K id, params Expression<Func<T, object>>[] includeProperties);
+        T FindSingle(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        Task<T> FindSingleAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        IQueryable<T> FindAll(params Expression<Func<T, object>>[] includeProperties);
+        IQueryable<T> FindAllAsync(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+    }
+}
