dee2e4e9dbaf531b5ed38b5191309cd3a566a00d
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-11-01 22:27:27 +0700

    configure cors/iss

---
+++ HumanResource/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace HumanResource.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(opstion =>
+            {
+                opstion.AddPolicy("CorePolicy", builder =>
+                {
+                    builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                });
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) {
+            services.Configure<IISOptions>(options =>
+            {
+
+            });    
+        }
+    }
+}
--- HumanResource/Program.cs
+++ HumanResource/Program.cs
@@ -1,6 +1,10 @@
+using HumanResource.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
