13325f9a25d110b390ebd9136da20e8c6be04981
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-11-02 09:43:13 +0700

    create Company/Employee and set relation

---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities.Models
+{
+    public class Employee
+    {
+        [Column("EmployeeId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "First Name is required.")]
+        [MaxLength(20,ErrorMessage ="Maximum length for the First Name is 20 characters.")]
+        public string FistName { get; set; }
+
+        [Required(ErrorMessage ="Last Nam is required.")]
+        [MaxLength(20,ErrorMessage ="Maximum length for the Last Name is 20 chareacters.")]
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+        public string Position { get; set; }
+
+        [ForeignKey(nameof(Company))]
+        public Guid CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
