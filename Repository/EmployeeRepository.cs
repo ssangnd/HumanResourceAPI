f681dd8182fa3900aef76d537e1a8478d7e2d6bf
ssangnd <75923274+ssangnd@users.noreply.github.com>
2022-11-16 10:23:52 +0700

    build extension for search/filter

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -3,6 +3,7 @@ using Entities.Models;
 using Entities.RequestFeature;
 using HumanResource.Infrastructure;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -15,9 +16,15 @@ namespace Repository
 
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChange)
         {
-            var employees = await FindAll(trackChange, e => e.CompanyId.Equals(companyId) &&
-               (e.Age >= employeeParameters.minAge && e.Age <= employeeParameters.maxAge))
-                .OrderBy(e=>e.FirstName)
+            //var employees = await FindAll(trackChange, e => e.CompanyId.Equals(companyId) &&
+            //   (e.Age >= employeeParameters.minAge && e.Age <= employeeParameters.maxAge)
+            //   && e.FirstName.Contains(employeeParameters.SearchTerm))
+            //    .OrderBy(e=>e.FirstName)
+            //    .ToListAsync();
+            var employees = await FindAll(trackChange, e => e.CompanyId.Equals(companyId))
+                .FilterEmployee(employeeParameters.minAge,employeeParameters.maxAge)
+                .Search(employeeParameters.SearchTerm)
+                .OrderBy(e => e.FirstName)
                 .ToListAsync();
 
             return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, 
---
+++ Repository/Extensions/RepositoryEmployeeExtension.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Extensions
+{
+    public static class RepositoryEmployeeExtension
+    {
+        public static IQueryable<Employee> FilterEmployee(this IQueryable<Employee> employees,
+            int minAge, int maxAge) => employees.Where(e => (e.Age >= minAge && e.Age <= maxAge));
+
+        public static IQueryable<Employee> Search (this IQueryable<Employee> employees, string searchTerm)
+        {
+            if (string.IsNullOrWhiteSpace(searchTerm)) return employees;
+            var lowerCaseTerm = searchTerm.Trim().ToLower();
+            return employees.Where(e => e.FirstName.ToLower().Contains(lowerCaseTerm));
+        }
+    }
+}
